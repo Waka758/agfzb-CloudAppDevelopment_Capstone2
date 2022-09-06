b6a4036100b5233c1a3468c68fca30c3c1e97993
Waka758 <88756617+Waka758@users.noreply.github.com>
2022-09-06 01:25:43 -0500

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "chefwilliejo.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "chefwilliejo.mybluemix.net", "chefwilliejo.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
